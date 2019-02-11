7237a2d9068c76298e8f875004e2db09cf993fe3
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-10 18:40:37 -0700

    Adds callback and signout routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
   resources :users, only: [:show]
+
+  get "/auth/github/callback" => "sessions#create"
+  get "/signout" => "sessions#destroy", :as => :signout
 end
