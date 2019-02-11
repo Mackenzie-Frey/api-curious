9490a39b449076f11886f61f135c0e3a26952f11
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-10 18:07:56 -0700

    Adds create method to model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
 
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+    end
+  end
+
 end
