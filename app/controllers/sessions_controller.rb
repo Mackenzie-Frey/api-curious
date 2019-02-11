f2ab1ff2b5d212f4ab5409609030ed53e9046ad9
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-10 18:40:16 -0700

    Adds create & destroy methods to sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,11 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_uid(auth["uid"]) || User.create_with_omniauth(auth)
-    session[:user_id] = user.id     redirect_to root_url, :notice => "Signed in!"
+    session[:user_id] = user.id
+    redirect_to user_path(user), :notice => "Signed in!"
   end
 
-  def destroyra
+  def destroy
     session[:user_id] = nil
     redirect_to root_url, :notice => "Signed out!"
   end
