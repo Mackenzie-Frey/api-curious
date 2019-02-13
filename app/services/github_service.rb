03eb8a54b81311cdd0a3f5c99183e117c72d8cb7
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-12 21:22:16 -0700

    Adds service.

---
+++ app/services/github_service.rb
@@ -0,0 +1,25 @@
+class GithubService
+
+  def initialize(user)
+    @token = user.token
+    binding.pry
+  end
+
+  def conn
+   url = "https://api.github.com"
+   Faraday.new(url: url) do |faraday|
+     faraday.adapter Faraday.default_adapter
+     faraday.headers["Authorization"] = "token #{@token}"
+   end
+  end
+
+  def repos
+    get_json('/user/repos')
+  end
+
+  def get_json(url)
+    response = conn.get(url)
+    results = JSON.parse(response.body, symbolize_names: true)
+  end
+
+end
