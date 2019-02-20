b523886ede2378b767854db28056b1e9aaee2326
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-20 08:30:21 -0700

    Adds model validation testing.

---
+++ spec/support/users_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  context 'Validations' do
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:uid)}
+    it { should validate_presence_of(:token)}
+  end
+
+  # context ''
+end
