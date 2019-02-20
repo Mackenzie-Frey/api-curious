e95bbd1f011dc83dc64af1975a6cfd9f04d4be7f
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-20 08:39:41 -0700

    Integrates model validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :uid
+  validates_presence_of :token
 
   def self.create_with_omniauth(auth)
     create! do |user|
