4c641c2a8a9a9ae017043916f290c8298c3bb7d5
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-02-20 08:43:34 -0700

    Adds comment.

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -5,7 +5,12 @@ class UserFacade
   end
 
   def repos
+    binding.pry
     GithubService.new(@user).repos
 # itereation through
+# Why isn't api call working? is it being called. Is a test calling it?
+# Do I need to create casettes?
+# What does the api testing docs show?
+# TDD? - probs start with TDD.
   end
 end
